56e6d6c94549f4c11679e9bf7ac4943112c3718a
Murillo Nogueira <murillo.nogueira01@outlook.com>
2023-08-23 19:33:48 -0300

    Adicionar arquivos de projeto.

---
+++ FilmFusion.App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FilmFusion.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
