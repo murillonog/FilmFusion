5552a28a711440ccd1de2c539454cd89a551a72d
Murillo Nogueira <murillo.nogueira01@outlook.com>
2023-08-24 18:16:43 -0300

    Criação do middleware de erro e identity user

---
+++ FilmFusion.Api/Middlewares/ErrorHandlingMiddleware.cs
@@ -0,0 +1,37 @@
+﻿namespace FilmFusion.Api.Middlewares
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ErrorHandlingMiddleware> _logger;
+
+        public ErrorHandlingMiddleware(RequestDelegate next, ILogger<ErrorHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next.Invoke(context);
+            }
+            catch (Exception ex)
+            {
+                await LogError(context, ex, statusCode: 500).ConfigureAwait(true);
+            }
+        }
+        private async Task LogError(HttpContext context, Exception ex, int statusCode)
+        {
+            _logger.LogError(ex, "<{traceIdentifier} | {message}>", context.TraceIdentifier, ex.Message);
+
+            if (!context.Response.HasStarted)
+            {
+                context.Response.StatusCode = statusCode;
+                context.Response.ContentType = @"application/json";
+
+                await context.Response.WriteAsync($"{{\"error\": \"{ex.Message}\"}}").ConfigureAwait(true);
+            }
+        }
+    }
+}
---
+++ FilmFusion.Infra.Data.SqlServer/Identity/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace FilmFusion.Infra.Data.SqlServer.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? Name { get; set; }
+        public string Email { get; set; }
+    }
+}
